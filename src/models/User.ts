5fdfdcc0590973b37591fdde6936cc01c6fd534d
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-21 09:52:28 -0400

    Created the user model

---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+import {Schema, model} from "mongoose"
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    emailAddress: {
+        type: String,
+        required: [true, "Email Address is required"]
+    },
+    phone: {
+        type: String,
+        required: [true, "Phone is required"],
+        maxlangth: 10,
+        minlangth: 10 
+    },
+    address: {
+        type: String,
+        required: [true, "Address is required"],  
+    },
+    wishList: {
+        type: Array,
+        required: [false],
+    }
+
+})
