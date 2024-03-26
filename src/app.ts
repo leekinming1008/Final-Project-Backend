4dc5d877a2d23f12d8ffd814be954b7fdc73af02
Angus Lee <zangus.lee@DFIretailgroup.com>
2024-03-25 21:19:30 -0400

    install cors

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import commentRouter from "./routes/commentRoutes";
 import categoryRoutes from "./routes/categoryRoutes";
 import postRouter from "./routes/postRoutes";
 import productRouter from "./routes/productRoutes";
+import cors from "cors";
 const app = express();
 
 // MIDDLEWARE for format the request body
@@ -11,6 +12,19 @@ app.use(express.json({limit: "10kb"}));
 
 // URL Encoder
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+// Setup the CORS
+var allowlist = ["http://localhost:5173", "https://angusproductmanagement.netlify.app"];
+var corsOptionsDelegate = function (req: any, callback: any) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+app.use(cors(corsOptionsDelegate));
 
 // Routing
 app.use("/api/user", userRouter);
